b660503c936970e8e183447e63ecabf897a0c86e
alisayimlar06-spec <alisayimlar06@gmail.com>
2026-05-14 21:27:25 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
+import os
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
-
-TOKEN = "7801801985:AAHx4tHrDahkSnEYMHENCqFDvreXbAZ8Fv0"
+                                                                                                              TOKEN = os.getenv("TOKEN")
+TOKEN = os.getenv("TOKEN")
 
 def start(update: Update, context: CallbackContext):
     update.message.reply_text("LastZ kod botu aktif!")
