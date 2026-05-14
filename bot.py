0201f94b56a739cc12844c9d8acb381eb6f51fd7
alisayimlar06-spec <alisayimlar06@gmail.com>
2026-05-14 18:18:06 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,15 @@
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
+
+TOKEN = 7801801985:AAHx4tHrDahkSnEYMHENCqFDvreXbAZ8Fv0
+
+def start(update: Update, context: CallbackContext):
+    update.message.reply_text("LastZ kod botu aktif!")
+
+updater = Updater(TOKEN, use_context=True)
+
+dp = updater.dispatcher
+dp.add_handler(CommandHandler("start", start))
+
+updater.start_polling()
+updater.idle()
